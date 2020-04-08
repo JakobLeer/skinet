de5a7cadf9618f7e7c4de4ae0bb41a18edf443fe
Jakob Leer <jpleer@tdcadsl.dk>
2020-04-08 14:18:55 +0200

    Clear up Startup.cs
    Using new Extensions.

---
+++ API/Extensions/ApplicationServicesExtensions.cs
@@ -0,0 +1,38 @@
+using System.Linq;
+using API.Errors;
+using Core.Interfaces;
+using Infrastructure.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace API.Extensions
+{
+    public static class ApplicationServicesExtensions
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
+            services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
+
+            services.Configure<ApiBehaviorOptions>(options =>
+                {
+                    options.InvalidModelStateResponseFactory = actionContext =>
+                    {
+                        var errors = actionContext.ModelState
+                                        .Where (m => m.Value.Errors.Count > 0)
+                                        .SelectMany(m => m.Value.Errors)
+                                        .Select(e => e.ErrorMessage).ToArray();
+                        
+                        var validationError = new ApiValidationError()
+                        {
+                            Errors = errors
+                        };
+
+                        return new BadRequestObjectResult(validationError);
+                    };
+                }
+            );
+
+            return services;
+        }
+    }
+}
--- API/Startup.cs
+++ API/Startup.cs
@@ -1,18 +1,13 @@
-using System.Linq;
-using API.Errors;
+using API.Extensions;
 using API.Helpers;
 using API.Middleware;
 using AutoMapper;
-using Core.Interfaces;
 using Infrastructure.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using Microsoft.OpenApi.Models;
 
 namespace API
 {
@@ -29,34 +24,10 @@ namespace API
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
-
-            services.Configure<ApiBehaviorOptions>(options =>
-                {
-                    options.InvalidModelStateResponseFactory = actionContext =>
-                    {
-                        var errors = actionContext.ModelState
-                                        .Where (m => m.Value.Errors.Count > 0)
-                                        .SelectMany(m => m.Value.Errors)
-                                        .Select(e => e.ErrorMessage).ToArray();
-                        
-                        var validationError = new ApiValidationError()
-                        {
-                            Errors = errors
-                        };
-
-                        return new BadRequestObjectResult(validationError);
-                    };
-                }
-            );
-
             services.AddDbContext<StoreContext>(options => options.UseSqlite(_config.GetConnectionString("Default")));
-            services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
             services.AddAutoMapper(typeof(MappingProfiles));
-
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo {Title = "SkiNet API", Version = "v1"});
-            });
+            services.AddApplicationServices();
+            services.AddSwaggerDocumentation();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -72,8 +43,7 @@ namespace API
 
             app.UseAuthorization();
 
-            app.UseSwagger();
-            app.UseSwaggerUI(config => config.SwaggerEndpoint("/swagger/v1/swagger.json", "SkiNet API v1"));
+            app.UseSwaggerDocumentation();
 
             app.UseEndpoints(endpoints =>
             {
