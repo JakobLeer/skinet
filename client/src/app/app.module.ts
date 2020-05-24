e3df549899ae5f024c744f1cd5a30a427a043235
Jakob Leer <jpleer@tdcadsl.dk>
2020-05-24 12:22:12 +0200

    UI: Add loading indicator
    - Install loading indicators from 'npm install ngx-spinner'
    - Import in AppModule
    - Add to top of AppComponent HTML
    - Add BusyService to turn spinner on and off
    - Add LoadingInterceptor to show spinner before starting a request and
      hide it, when all busy stuff has completed.
      You can fake a delay by putting it into the interceptor.
    - Add LoadingInterceptor to AppModule.

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { NgxSpinnerModule } from 'ngx-spinner';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { CoreModule } from './core/core.module';
+import { LoadingInterceptor } from './core/interceptors/loading.interceptor';
 
 @NgModule({
   declarations: [
@@ -17,10 +19,12 @@ import { CoreModule } from './core/core.module';
     BrowserAnimationsModule,
     BrowserModule,
     CoreModule,
-    HttpClientModule
+    HttpClientModule,
+    NgxSpinnerModule
   ],
   providers: [
-    { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true }
+    { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
+    { provide: HTTP_INTERCEPTORS, useClass: LoadingInterceptor, multi: true }
   ],
   bootstrap: [AppComponent]
 })
