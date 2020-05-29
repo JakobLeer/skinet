46d4fea2b2d33f36103bbcad6ece4c23a59bdc33
Jakob Leer <jpleer@tdcadsl.dk>
2020-05-29 18:22:03 +0200

    UI: Create BasketService
    - expose basket as an Observable (basket$) based on a Subject (basketSource)
      to allow multicasting the basket content.
    - use a BehaviorSubject to set intial value.
    - update the basket$ by calling next on basketSource.

--- client/src/app/basket/basket.service.ts
+++ client/src/app/basket/basket.service.ts
@@ -1,9 +1,41 @@
+import { BehaviorSubject } from 'rxjs';
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+
+import { environment } from 'src/environments/environment';
+import { IBasket } from '../shared/Models/basket';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BasketService {
+  baseUrl = environment.apiUrl;
 
-  constructor() { }
+  private basketSource = new BehaviorSubject<IBasket>(null);
+  basket$ = this.basketSource.asObservable();
+
+  constructor(private http: HttpClient) {}
+
+  getBasket(id: string) {
+    return this.http.get<IBasket>(`${this.baseUrl}/basket?id=${id}`)
+      .pipe(
+        map(basket => {
+          this.basketSource.next(basket);
+        })
+      );
+  }
+
+  setBasket(basket: IBasket) {
+    this.http.post<IBasket>(`${this.baseUrl}/basket`, basket)
+        .subscribe(response => {
+          this.basketSource.next(response);
+        }, error => {
+          console.log(error);
+        });
+  }
+
+  getCurrentBasket() {
+    return this.basketSource.value;
+  }
 }
