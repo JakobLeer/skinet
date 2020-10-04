166cc8d94ea89ce052c064e051c2c488b14613b0
Jakob Leer <jpleer@tdcadsl.dk>
2020-10-04 08:22:40 +0200

    UI: Create lazy loaded Account module - Module: in src/app run ng g m account - cd account - Routing: ng g m account-routing --flat - Service: ng g s account --flat --skip-tests - Component Login: ng g c login --skip-tests - Component Register: ng g c register --skip-tests - Routing module: Setup routes to Login and Register - App routing: Add new module as lazy loaded

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -57,6 +57,13 @@ const routes: Routes = [
     }
   },
   {
+    path: 'account',
+    loadChildren: () => import('./account/account.module').then(mod => mod.AccountModule),
+    data: {
+      breadcrumb: { skip: true }
+    }
+  },
+  {
     path: '**',
     redirectTo: 'not-found',
     pathMatch: 'full'
